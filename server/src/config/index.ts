f15b30f22b86d1ed45b74114efdfb53d2f061508
Perry Smith-Moss <perrysmithmoss@gmail.com>
2026-03-28 13:45:09 +0000

    refactor: use zod for config envs

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,9 +1,28 @@
-import { IAppConfig } from "@shared/interfaces/config.interface";
-import { EnvValidator } from "./env";
+import { z } from "zod";
 
-const env = EnvValidator.validate();
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(["development", "test", "production"])
+    .default("development"),
+  PORT: z.coerce.number().default(5500),
+  HOST: z.string().default("localhost"),
+  ENABLE_FLEET_SIMULATOR: z
+    .enum(["true", "false"])
+    .default("false")
+    .transform((val) => val === "true"),
+});
 
-export const config: IAppConfig = {
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  console.error("❌ Invalid Environment Configuration:");
+  console.error(JSON.stringify(z.treeifyError(_env.error), null, 2));
+  process.exit(1);
+}
+
+const env = _env.data;
+
+export const config = {
   server: {
     port: env.PORT,
     host: env.HOST,
@@ -11,4 +30,14 @@ export const config: IAppConfig = {
     isProd: env.NODE_ENV === "production",
     isDev: env.NODE_ENV === "development",
   },
+  modules: {
+    vehicle: {
+      seedMockData: env.ENABLE_FLEET_SIMULATOR,
+    },
+    fleet: {
+      enableFleetSimulator: env.ENABLE_FLEET_SIMULATOR,
+    },
+  },
 } as const;
+
+export type IAppConfig = typeof config;
